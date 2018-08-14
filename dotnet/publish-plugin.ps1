59071c86f7c01b53e602dd068dcaa347465f37d0
Matthias Koch <ithrowexceptions@gmail.com>
2018-08-14 14:59:30 +0200

    ++

--- dotnet/publish-plugin.ps1
+++ dotnet/publish-plugin.ps1
@@ -12,19 +12,22 @@ Param(
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 $OutputDirectory = "$PSScriptRoot\output"
 
+# ReSharper
 Remove-Item $OutputDirectory -Force -Recurse
 & dotnet pack SamplePlugin.sln /p:PackageVersion=$Version --configuration $Configuration --output $OutputDirectory # --NoPackageAnalysis
-
 $ReSharperPackageFile = Get-ChildItem "$OutputDirectory\SamplePlugin.ReSharper.*.nupkg"
 
+# Rider
 $RiderOutputDirectory = "$OutputDirectory\rider-SamplePlugin"
 $RiderPluginXml = [xml] (Get-Content "$PSScriptRoot\src\plugin.xml")
 $RiderPluginId = $RiderPluginXml."idea-plugin".id
 
+# Rider :: dotnet
 mkdir "$RiderOutputDirectory" > $null
 mkdir "$RiderOutputDirectory\dotnet" > $null
 Copy-Item -Path "$PSScriptRoot\src\SamplePlugin.ReSharper\bin\SamplePlugin.Rider\$Configuration\SamplePlugin.Rider.*" -Destination "$RiderOutputDirectory\dotnet"
 
+# Rider :: idea
 $WaveSpec = ([xml] (Get-Content "$PSScriptRoot\src\Versions.props")).Project.PropertyGroup.SdkVersion
 $WaveSpec = "$($WaveSpec.Substring(2,2))$($WaveSpec.Substring(5,1))"
 $RiderPluginXml."idea-plugin".version = $Version
@@ -33,8 +36,12 @@ $RiderPluginXml."idea-plugin"."idea-version".setAttribute("until-build", "$WaveS
 mkdir "$RiderOutputDirectory\META-INF" > $null
 $RiderPluginXml.Save("$RiderOutputDirectory\META-INF\plugin.xml")
 
-$RiderPackageFile = "$OutputDirectory\rider-SamplePlugin.zip"
+# Rider :: zip
+$RiderPackageFile = "$OutputDirectory\$RiderPluginid.zip"
 & "$PSScriptRoot\..\tools\7za.exe" a "$RiderPackageFile" "$RiderOutputDirectory" > $null
 
+# Publish
 Write-Host dotnet nuget push $ReSharperPackageFile --source "https://resharper-plugins.jetbrains.com/api/v2/package" --api-key $ReSharperApiKey
 Write-Host java -jar "$PSScriptRoot\..\tools\plugin-repository-rest-client.jar" upload -file "$RiderPackageFile" -plugin "$RiderPluginId" -username $RiderUsername -password $RiderPassword
+
+# git tag $Version
